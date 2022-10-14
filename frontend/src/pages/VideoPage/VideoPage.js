c1923cb9ae08418d03187d284d0b98272e0e5dd9
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-10-14 11:10:46 -0400

    related video is now appearing under main video on VideoPage

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -1,9 +1,39 @@
-//import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { KEY } from "../../localKey";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const VideoPage = () => {;
-   
+    //const [comments, setComments] = useState("")
     const { id } = useParams();
+    const [videos, setVideos] = useState("")
+
+    useEffect(() => {
+        //fetchComments();
+        fetchRelatedVideos();
+    }, [])
+
+{/*const fetchComments = async () => {
+    try {
+        let response = await axios.get(`http://127.0.0.1:8000/api/comment/${id}`
+        );
+        setComments(response.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+} */}
+
+const fetchRelatedVideos = async (search = "nba") => {
+    try {
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${id}&type=video&part=snippet&key=${KEY}`
+        );
+        setVideos(response.data.items);
+        console.log(response.data.items)
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
 
 
 return (
@@ -13,9 +43,25 @@ return (
   src={`http://www.youtube.com/embed/{id}`}
   frameborder="0"></iframe>
         </div>
-
+        <div>
+        {videos && videos.map((video) => (
+          <p key={video.id.videoId}>
+           Title: {video.snippet.title} 
+           <br />
+           <Link to={`/videos/${video.id.videoId}`}>
+            <img src={video.snippet.thumbnails.medium.url} />
+           </Link>
+            <br />
+           Description: {video.snippet.description}
+           <br />
+           <br />
+           <br />
+          </p>
+        ))}
+        </div>
     </div>
 )
 }
+
 
 export default VideoPage;
